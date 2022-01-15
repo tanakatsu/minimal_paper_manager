534f7ffd8c32f3dadab16fa8a2213978f400c779
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2022-01-15 13:34:58 +0900

    Remove missing files from catalog

--- make_catalog.py
+++ make_catalog.py
@@ -5,31 +5,65 @@ from tqdm import tqdm
 from paper_meta_info import PaperMetaInfo
 
 
+COLUMNS = ["filename", "dirpath", "title", "abstract", "filepath"]
+
+
+def remove_missing_files(df, keep=False):
+    missing = df["filepath"].apply(lambda x: not os.path.exists(x))
+    df_missing = df[missing]
+    if len(df_missing):
+        print(f"Found {len(df_missing)} missing files in catalog.")
+        for items in df_missing.itertuples():
+            print(f"  missing: {items.filepath}")
+        if not keep:
+            print("  Removed from catalog")
+    if keep:
+        return df
+    return df[~missing]
+
+
 def main():
     parser = ArgumentParser()
-    parser.add_argument('-o', '--output', type=str, default="catalog.csv")
+    parser.add_argument('-f', '--file', type=str, default="catalog.csv")
+    parser.add_argument('--keep_missing', action="store_true")
     parser.add_argument('document_root_dir', type=str, help='document root directory')
     args = parser.parse_args()
 
+    catalog_file = args.file
+    if os.path.exists(catalog_file):
+        df = pd.read_csv(catalog_file)
+    else:
+        df = pd.DataFrame(columns=COLUMNS)
+
+    # 削除されたレコードの検出
+    df = remove_missing_files(df, keep=args.keep_missing)
+
     file_list = []
-    for curDir, dirs, files in os.walk(args.document_root_dir):
-        dirpath = curDir.replace(args.document_root_dir, "").lstrip("/")
+    root_dir = os.path.realpath(args.document_root_dir)
+    for curDir, dirs, files in os.walk(root_dir):
+        dirpath = curDir.replace(root_dir, "").lstrip("/")
         for filename in files:
             if filename.endswith(".pdf"):
-                filepath = os.path.join(args.document_root_dir, dirpath, filename)
+                filepath = os.path.join(root_dir, dirpath, filename)
                 file_list.append({'filename': filename, 'dirpath': dirpath, 'filepath': filepath})
-    print(f"Found {len(file_list)} papers")
+    print(f"Found {len(file_list)} papers in target directory.")
 
     print("Extracting title and abstract...")
     extractor = PaperMetaInfo()
     for fileinfo in tqdm(file_list):
         print(fileinfo['filepath'])
-        title, abstract = extractor.get_title_and_abstract(fileinfo['filepath'])
-        fileinfo["title"] = title
-        fileinfo["abstract"] = abstract
+        df_matched = df[df['filepath'] == fileinfo['filepath']]
+        if len(df_matched):
+            # カタログ上のデータを再利用する
+            fileinfo["title"] = df_matched.iloc[0]['title']
+            fileinfo["abstract"] = df_matched.iloc[0]['abstract']
+        else:
+            title, abstract = extractor.get_title_and_abstract(fileinfo['filepath'])
+            fileinfo["title"] = title
+            fileinfo["abstract"] = abstract
 
-    df = pd.DataFrame(file_list, columns=["filename", "dirpath", "title", "abstract", "filepath"])
-    df.to_csv(args.output, index=False)
+    df_out = pd.DataFrame(file_list, columns=COLUMNS)
+    df_out.to_csv(catalog_file, index=False)
     print("Finished.")
 
 if __name__ == "__main__":
