333ec211c8458374298cbd60f72339cea2360afd
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2022-01-15 14:52:58 +0900

    Add error message

--- test_onefile.py
+++ test_onefile.py
@@ -1,6 +1,10 @@
 import sys
 from paper_meta_info import PaperMetaInfo
 
+if not len(sys.argv) == 2:
+    print("Usage: python test_onefile.py pdf_file")
+    sys.exit(1)
+
 pdf_file = sys.argv[1]
 extractor = PaperMetaInfo(debug=True)
 title, abstract = extractor.get_title_and_abstract(pdf_file)
